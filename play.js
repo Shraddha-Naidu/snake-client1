2ade9f38eab71a31890965473214337a5acae7cc
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-16 22:14:08 -0600

    Setup to handle user input

--- play.js
+++ play.js
@@ -5,3 +5,25 @@ PORT: 50541
 */
 
 const { connect } = require('./client')
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", key => {})
+  return stdin;
+};
+
+const handleUserInput = (key) {
+  const stdout = process.stdout;
+  if (key ==='\u0003') {
+    stdout.write(`Exiting 🐍. Bye...\n`);
+    process.exit();
+  }
+};
+
+setupInput();
+handleUserInput();
