6aeb8bcbae9dd247991bebfcef6483c661ac7fc1
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-16 22:34:57 -0600

    Added input file

--- play.js
+++ play.js
@@ -5,27 +5,4 @@ PORT: 50541
 */
 
 const { connect } = require('./client')
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", key => {
-  handleUserInput(key);
-  });
-  return stdin;
-};
-
-const handleUserInput = (key) {
-  const stdout = process.stdout;
-  if (key ==='\u0003') {
-    stdout.write(`Exiting 🐍. Bye...\n`);
-    process.exit();
-  }
-};
-
-setupInput();
-handleUserInput();
+const{ setupInput } = require('./input')
