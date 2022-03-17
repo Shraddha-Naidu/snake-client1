1094908e192ab67a49db03c26b15ae9cbce5d83a
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-16 23:24:48 -0600

    Updated for constants and linted

--- client.js
+++ client.js
@@ -1,29 +1,30 @@
 //connect function sets up a connection and returns an object
 const net = require("net");//States tha net package is reguired to run. built in
+const { IP,PORT } = require('./constants');
 
 //establishes connection with game server
-const connect =  function () {
+const connect =  function() {
   const connection = net.createConnection({
-    host: "165.227.47.243",//IP address here in ""
-    port: 50541//PORT Number here
+    host: IP,//IP address here in ""
+    port: PORT//PORT Number here
   });
 
   // interprets incoming data to text (decodes)
   connection.setEncoding("utf-8");
-  
-  
+
   connection.on("connect", () => {
     console.log(`Connected to 🐍 game server!`);
-    connection.write("Name: SN");
-    connection.write("Move: up");
+    connection.write(`Name: SN`);//Does not recognise all caps Name or Move, returns with "Huh!"
+    //connection.write("Move: up");
   });
 
   connection.on("data", message => {
-    console.log(message)
-  });//on to receive data
+    console.log(message);
+  });
 
   return connection;
 };
+
 
 //EXPORT
 module.exports = { connect };
