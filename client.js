15af5aaae9fe3395521a0841ae520844d9dc9b82
Shraddha Naidu <naidu.shraddha@gmail.com>
2022-03-16 18:08:26 -0600

    Transferred connect function and connected function using ES6 shorthand

---
+++ client.js
@@ -0,0 +1,25 @@
+//connect function sets up a connection and returns an object
+const net = require("net");//States tha net package is reguired to run. built in
+
+//establishes connection with game server
+const connect =  function () {
+  const connection = net.createConnection({
+    host: "165.227.47.243",//IP address here in ""
+    port: 50541//PORT Number here
+  });
+
+  // interprets incoming data to text (decodes)
+  connection.setEncoding("utf-8");
+
+  connection.on("data", message => {
+    console.log(message)
+  });//on to receive data
+
+  return connection;
+};
+
+console.log("Connecting ... ⏳")//Prints to console indicating that connection to server is being made
+connect();
+
+//EXPORT
+module.exports = { connect };
